16e0829e49cd8d6df156e01332a6c96262b597ee
neel-singhania <neel@iitk.ac.in>
2021-11-13 16:57:29 +0530

    updates

--- gym-env/gym_env/envs/__init__.py
+++ gym-env/gym_env/envs/__init__.py
@@ -2,4 +2,4 @@ from gym_env.envs.foraging0 import Foraging0
 from gym_env.envs.foraging1 import Foraging1
 from gym_env.envs.foraging2 import Foraging2
 from gym_env.envs.foraging import Foraging
-from gym_env.envs.richpatch_poorpatch import ForagingRichPoorPatch
+from gym_env.envs.richPatch_poorPatch import ForagingRichPoorPatch
